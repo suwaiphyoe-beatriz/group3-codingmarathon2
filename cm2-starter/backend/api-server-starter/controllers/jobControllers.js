a3c566d4a37e6814941e3eaed0de112639a65d16
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-26 11:24:05 +0300

    iteration 4 backend jobs& answers

--- cm2-starter/backend/api-server-starter/controllers/jobControllers.js
+++ cm2-starter/backend/api-server-starter/controllers/jobControllers.js
@@ -1,9 +1,23 @@
 const Job = require("../models/jobModel"); 
 const mongoose = require("mongoose");
-
+/*
 const getAllJobs = async (req, res) => {
   try {
     const jobs = await Job.find({}).sort({ createdAt: -1 });
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve jobs" });
+  }
+};
+*/
+const getAllJobs = async (req, res) => {
+  try {
+    const limit = parseInt(req.query._limit);
+
+    const jobs = limit
+      ? await Job.find({}).sort({ createdAt: -1 }).limit(limit)
+      : await Job.find({}).sort({ createdAt: -1 });
+
     res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({ message: "Failed to retrieve jobs" });
