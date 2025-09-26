01ee05c809748e9ea052b34f8454faf202dad22f
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-26 10:19:01 +0300

    finished iteration 2 backend

--- cm2-starter/frontend/vite.config.js
+++ cm2-starter/frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-
+/*
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,17 +17,17 @@ export default defineConfig({
     },
   },
 });
- 
+*/
 
-// export default defineConfig({
-  // plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:4000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
